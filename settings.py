236c5168a39d09d2889314cbdb79d817bb1c4802
hey-sk <stephenakeane@gmail.com>
2023-04-28 22:24:41 -0400

    initial commit

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.screen_size = (self.screen_width, self.screen_height)
+        self.bg_color = (40, 40, 40)
+        
